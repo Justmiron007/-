734b0f4930eba71660e3746d0bac277747dcaca3
Justmiron007 <raen.96@mail.ru>
2024-11-16 21:56:10 +0500

    main.py

--- main.py
+++ main.py
@@ -5,17 +5,9 @@ print(9 ** 0.5 * 5)
 print(9.99 > 9.98 and 1000 != 1000.1)
 
 # 3rd program
-result_no_priority = 2 * 2 + 2
-result_with_priority = 2 * (2 + 2)
-comparison_result = result_no_priority == result_with_priority
-print("Без приоритета:", result_no_priority)
-print("С приоритетом:", result_with_priority)
-print("Результаты равны?", comparison_result)
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2 == 2 * (2 + 2))
 
 # 4th program
-s = '123.456'
-num = float(s)
-num_shifted = num * 10
-num_int = int(num_shifted)
-first_after_decimal = num_int % 10
-print(first_after_decimal)
+print(int(float('123.456') * 10) % 10)
