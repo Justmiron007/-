4ee0283add5c3dc05920d8edf4ed97bd427aa7ae
Justmiron007 <raen.96@mail.ru>
2024-11-16 21:53:59 +0500

    main.py

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+# 1st program
+print(9 ** 0.5 * 5)
 
-primes = []
-not_primes = []
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
 
-for number in numbers:
-    if number == 1:
-        continue
-    is_prime = True
-    for i in range(2, number):
-        if number % i == 0:
-            is_prime = False
-            break
-    if is_prime:
-        primes.append(number)
-    else:
-        not_primes.append(number)
+# 3rd program
+result_no_priority = 2 * 2 + 2
+result_with_priority = 2 * (2 + 2)
+comparison_result = result_no_priority == result_with_priority
+print("Без приоритета:", result_no_priority)
+print("С приоритетом:", result_with_priority)
+print("Результаты равны?", comparison_result)
 
-print("Primes:", primes)
-print("Not Primes:", not_primes)
+# 4th program
+s = '123.456'
+num = float(s)
+num_shifted = num * 10
+num_int = int(num_shifted)
+first_after_decimal = num_int % 10
+print(first_after_decimal)
